8fc9921fc2345450ec38f3a383aa39ad00e18b74
Sujal Arora <arunarorasujal@gmail.com>
2026-03-20 16:40:40 +0530

    added ex1-8

---
+++ ex1-8.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int c;
+    long nb = 0, nt = 0, nl = 0;
+
+    while ((c = getchar()) != EOF)
+    {
+        if (c == ' ')
+            nb++;
+        if (c == '\t')
+            nt++;
+        if (c == '\n')
+            nl++;
+    }
+
+    printf("Blanks = %ld\nTabs = %ld\nNewlines = %ld\n", nb, nt, nl);
+
+
+}
