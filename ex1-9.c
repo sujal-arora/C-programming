e5e265c6ba3d6f6a164dcbf83963f9c7217efebe
Sujal Arora <arunarorasujal@gmail.com>
2026-03-20 17:18:33 +0530

    addedex1-9

---
+++ ex1-9.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int c;
+    int prevblank = 0;   // flag to check previous character
+
+    while ((c = getchar()) != EOF)
+    {
+        if (c == ' ')
+        {
+            if (prevblank == 0)
+            {
+                putchar(c);
+                prevblank = 1;
+            }
+        }
+        else
+        {
+            putchar(c);
+            prevblank = 0;
+        }
+    }
+
+    return 0;
+}
